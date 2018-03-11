ea041eb90a51820fea9ce01f40017d9ee5160625
vagrant <vagrant@homestead>
2018-03-11 16:38:55 +0000

    fix migrations

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,53 +16,53 @@
         integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
         crossorigin="anonymous"></script>
 <script>
-    // $.getJSON('data.json', function (objects) {
-    //     objects.forEach((item, index) => {
-    //
-    //         let street = (item.address.street).replace(/ /g, "+"),
-    //             city = 'Beograd',
-    //             url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + street + ',' + city + '&key=AIzaSyDO5z2fZjTYcTi8fPbiYaFlQcSFDMY3LhI';
-    //
-    //         axios.get(url).then(response => {
-    //             let data = {
-    //                 name: item.shortTitle,
-    //                 address: item.address.street,
-    //                 city: city,
-    //                 zip: item.ptt,
-    //                 phone: item.phone,
-    //                 lat: response.data.results[0].geometry.location.lat,
-    //                 lng: response.data.results[0].geometry.location.lng
-    //             };
-    //             axios.post('/api/betting', data).then(response => {
-    //                 console.log('success');
-    //             });
-    //         });
-    //     });
-    // });
+    $.getJSON('data.json', function (objects) {
+        objects.forEach((item, index) => {
+
+            let street = (item.address.street).replace(/ /g, "+"),
+                city = 'Beograd',
+                url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + street + ',' + city + '&key=AIzaSyDO5z2fZjTYcTi8fPbiYaFlQcSFDMY3LhI';
+
+            axios.get(url).then(response => {
+                let data = {
+                    name: item.shortTitle,
+                    address: item.address.street,
+                    city: city,
+                    zip: item.ptt,
+                    phone: item.phone,
+                    lat: response.data.results[0].geometry.location.lat,
+                    lng: response.data.results[0].geometry.location.lng
+                };
+                axios.post('/api/betting', data).then(response => {
+                    console.log('success');
+                });
+            });
+        });
+    });
 </script>
 <script>
 
-    $.getJSON('elementarySchool.json', function (item) {
-        item.forEach(function (element) {
-            if (element.naziv_su_cir === 'Београд') {
-
-                let latlng = element.gps.split(",");
-                let data = {
-                    name: element.naziv_skole,
-                    address: element.adresa,
-                    city: 'Beograd',
-                    zip: element.postanski_broj,
-                    phone: element.tel,
-                    lat: latlng[0],
-                    lng: latlng[1],
-                    type: 0
-                };
-                axios.post('/api/schools', data).then(response => {
-                    console.log('success');
-                });
-            }
-        });
-    });
+    // $.getJSON('elementarySchool.json', function (item) {
+    //     item.forEach(function (element) {
+    //         if (element.naziv_su_cir === 'Београд') {
+    //
+    //             let latlng = element.gps.split(",");
+    //             let data = {
+    //                 name: element.naziv_skole,
+    //                 address: element.adresa,
+    //                 city: 'Beograd',
+    //                 zip: element.postanski_broj,
+    //                 phone: element.tel,
+    //                 lat: latlng[0],
+    //                 lng: latlng[1],
+    //                 type: 0
+    //             };
+    //             axios.post('/api/schools', data).then(response => {
+    //                 console.log('success');
+    //             });
+    //         }
+    //     });
+    // });
 
     //
     // $.getJSON('highSchool.json', function (data) {
